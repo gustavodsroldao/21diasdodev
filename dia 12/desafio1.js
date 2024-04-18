12e9081bf9e61f4aaeba8b3f9fbdcb528e798360
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-04-17 23:09:03 -0300

    erros corrigidos.

--- dia 12/desafio1.js
+++ dia 12/desafio1.js
@@ -11,65 +11,64 @@ while (continuar) {
   // a execução do código com um switchCase dentro de um while
   let opcao = prompt("O que deseja fazer? 1 - Cadastro/ 2 - Login / 3 - Excluir / 4 - Encerrar");
 
-  swtich(opcao) {
-  case "1":
-    //passo 4: criando um cadastro de usario
-    nomes[contador] = prompt("Qual o nome?")
-    senhas[contador] = prompt("Qual a senha?")
-    contador++
+  switch(opcao) {
+    case "1":
+      //passo 4: criando um cadastro de usario
+      nomes[contador] = prompt("Qual o nome?")
+      senhas[contador] = prompt("Qual a senha?")
+      contador++
 
-    break;
-  
+      break;
+    
+    case "2":
+      //passo 5 criando login
+      let nome = prompt("Qual o nome?")
+      let senha = promtp("Qual a senha?")
+      let loginValido = false
 
-  case "2":
-    //passo 5 criando login
-    let nome = prompt("Qual o nome?")
-    let senha = promtp("Qual a senha?")
-    let loginValido = false
-
-    for (let i = 0; i < nomes.lenght; i++) {
-      if (nome == nomes[i] && senha == senhas[i]) {
-        loginValido = true
+      for (let i = 0; i < nomes.lenght; i++) {
+        if (nome == nomes[i] && senha == senhas[i]) {
+          loginValido = true
+        }
       }
-    }
 
-    if (loginValido) {
-      alert("Login feito com sucesso. Bem vindo!")
-    } else {
-      alert("Login ou senha incorretos!")
-    }
-
-    break;
-
-  case "3":
-    //passo 6: criando opc excluir
-    let nomeExcluir = prompt("Qual o nomne que deseja excluir?")
-    let nomesAux = []
-    let senhasAux = []
-    let contadorAux = 0
-
-    for (let i = 0; i < contador; i++) {
-      if (nomeExcluir == nomes[i]) {
-        alert("Cadastro excluído com sucesso")
+      if (loginValido) {
+        alert("Login feito com sucesso. Bem vindo!")
       } else {
-        nomesAux[contadorAux] = nomes[i]
-        senhasAux[contadorAux] = senhas[i]
-        contadorAux++
+        alert("Login ou senha incorretos!")
       }
-    }
 
-    nomes = nomesAux
-    senhas = senhasAux
-    contador--
+      break;
 
-    break;
+    case "3":
+        //passo 6: criando opc excluir
+      let nomeExcluir = prompt("Qual o nomne que deseja excluir?")
+      let nomesAux = []
+      let senhasAux = []
+      let contadorAux = 0
+
+      for (let i = 0; i < contador; i++) {
+        if (nomeExcluir == nomes[i]) {
+          alert("Cadastro excluído com sucesso")
+        } else {
+          nomesAux[contadorAux] = nomes[i]
+          senhasAux[contadorAux] = senhas[i]
+          contadorAux++
+        }
+      }
+
+      nomes = nomesAux
+      senhas = senhasAux
+      contador--
+
+      break;
           
-  case "4":
-    continuar = false
-    break;
-  default:
-    console.log("Opção invalida.")
-    break;
+    case "4":
+      continuar = false
+      break;
+    default:
+      console.log("Opção invalida.")
+      break;
 
   }
 }
